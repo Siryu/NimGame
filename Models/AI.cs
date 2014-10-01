acfb057c1b5abf1158daa158a1a40112d04b4cfc
Siryu <coreymassey@gmail.com>
2014-09-30 20:30:40 -0600

    AI setup
    Finished what i think will work as an AI, there was no testing, just
    what i thought about on paper... please make changes or fixes or
    something, thanks

--- Models/AI.cs
+++ Models/AI.cs
@@ -9,6 +9,7 @@ namespace NimGame.Models
     public class AI
     {
         List<Move> movesPossible;
+        Random rand = new Random();
 
         public AI()
         {
@@ -17,26 +18,60 @@ namespace NimGame.Models
 
         public int[] calculateTurn(int[] boardState)
         {
+            Move returnMove = new Move(boardState);
+
             foreach (Move m in movesPossible)
             {
                 if (m.BoardSetup == boardState)
                 {
-
+                    foreach (Move n in m.NextMove)
+                    {
+                        if (n.Value <= returnMove.Value)
+                        {
+                            if (n.BoardSetup[0] <= boardState[0] && n.BoardSetup[1] <= boardState[1] && n.BoardSetup[2] <= boardState[2])
+                            {
+                                returnMove = n;
+                            }
+                        }
+                    }
                 }
             }
+
+            if (returnMove.BoardSetup == boardState)
+            {
+                // needs some work on a random move play by a computer player.
+                
+                int row = rand.Next(3);
+                returnMove.BoardSetup[row] = returnMove.BoardSetup[row] - (returnMove.BoardSetup[row] - rand.Next(returnMove.BoardSetup[row]) + 1); 
+            }
+
+            return returnMove.BoardSetup;
         }
 
         public void AddGame(List<int[]> moves)
         {
-            foreach (int[] intarr in moves)
+            int win = moves.Count % 2 == 0 ? -1 : 1;
+
+            for(int i = 0; i < moves.Count; i++)
             {
-                foreach (int[] key in moveLog.Keys)
+                bool isThere = false;
+                float value = (moves.Count / i) * win;
+                foreach (Move m in movesPossible)
                 {
-                    if (key[0] == intarr[0] && key[1] == intarr[1] && key[2] == intarr[2])
+                    if (m.BoardSetup == moves[i])
                     {
-                        
-                    }
+                        isThere = true;
+                        m.IsMove(moves[i + 1], value);
+                    }                       
                 }
+                if (!isThere)
+                {
+                    Move newMove = new Move(moves[i]);
+                    newMove.IsMove(moves[i + 1], value);
+                    movesPossible.Add(newMove);
+                }
+
+                win *= -1;
             }
         }
     }
--- Models/Move.cs
+++ Models/Move.cs
@@ -11,12 +11,45 @@ namespace NimGame.Models
         public int[] BoardSetup { get; set; }
         public float Value { get; set; }
         public int TimesSelected { get; set; }
+        public List<Move> NextMove;
 
         public Move(int[] boardSetup)
         {
+            this.NextMove = new List<Move>();
             this.BoardSetup = boardSetup;
             this.Value = 0f;
             this.TimesSelected = 1;
+        }
+
+        public Move(int[] boardSetup, float value)
+        {
+            this.NextMove = new List<Move>();
+            this.BoardSetup = boardSetup;
+            this.Value = value;
+            this.TimesSelected = 1;
+        }
+
+        public void IsMove(int[] next, float value)
+        {
+            bool isAdded = false;
+            foreach (Move m in NextMove)
+            {
+                if (m.BoardSetup == next)
+                {
+                    float moveValue = m.Value * m.TimesSelected;
+                    moveValue += value;
+                    m.TimesSelected += 1;
+                    m.Value = moveValue / m.TimesSelected;
+                    isAdded = true;
+                }
+            }
+
+            if (!isAdded)
+            {
+                Move newMove = new Move(next);
+                newMove.Value = value;
+                this.NextMove.Add(newMove);
+            }
         }
     }
 }
