732eff3b81c0d4c0ce00c369d415b48136910b33
Siryu <coreymassey@gmail.com>
2014-10-03 08:45:18 -0600

    last update on original game before refactoring!
    now ai learns

--- Models/AI.cs
+++ Models/AI.cs
@@ -48,7 +48,7 @@ namespace NimGame.Models
 
         public void AddGame(List<int[]> moves)
         {
-            int win = moves.Count % 2 == 0 ? -1 : 1;
+            int win = moves.Count % 2 == 0 ? 1 : -1;
 
             for(int i = 0; i < moves.Count; i++)
             {
